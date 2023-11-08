2306986622173191a2c4e132d9e628fe3396dd67
Dirk Eddelbuettel <edd@debian.org>
2023-11-08 09:40:52 -0600

    Add R6 wrapper for reindexer

---
+++ apis/r/R/Reindexer.R
@@ -0,0 +1,39 @@
+#' Reindexer Class
+#'
+#' @keywords internal
+#' @export
+
+Reindexer <- R6::R6Class(
+    classname = "Reindexer",
+    public = list(
+
+        #' @description Initialize a Reindexer object
+        #'
+        #' @param map_keys An integer64 vector with keys to be mapped by the class
+        #' @param nb_threads The number of threads to use, defaults to four
+        initialize = function(map_keys, nb_threads = 4) {
+            stopifnot("Keys must be 'integer64'" = is.integer64(map_keys))
+            private$idxptr <- reindex_create()
+            private$threadcount <- nb_threads
+            reindex_map(private$idxptr, map_keys)
+        },
+
+        #' @description Lookup a vector of keys
+        #'
+        #' @param lookup_keys An integer64 vector with values to be looked up
+        #' @return A vector of the same length with index positions
+        lookup = function(lookup_keys) {
+            ## catch vulnerability in indexer
+            stopifnot("Keys must outnumber thread count" = length(lookup_keys) > private$threadcount,
+                      "Keys must be 'integer64'" = is.integer64(lookup_keys))
+            reindex_lookup(private$idxptr, lookup_keys)
+        }
+    ),
+
+    private = list(
+        ## Internal 'external pointer object holding an Reindexer class instance
+        idxptr = NULL,
+        ## Internal thread counter
+        threadcount = NULL
+    )
+)
--- apis/r/tests/testthat/test-reindex.R
+++ apis/r/tests/testthat/test-reindex.R
@@ -1,22 +1,20 @@
+test_that("Reindexing", {
+    skip_if(!extended_tests())
 
-suppressMessages({
-    library(bit64)
-    library(tiledbsoma)
+    expect_error(Reindexer$new())
+    expect_error(Reindexer$new(c(1,3,5), 4))
+
+    init_map <- bit64::as.integer64(c(32, 10 ,12, 5))
+    expect_silent(reind <- Reindexer$new(init_map))
+
+    init_keys <- bit64::as.integer64(c(5, 12, 10, 32, 10 ,12, 5, 5, 5))
+    exp_res <- bit64::as.integer64(c(3, 2, 1, 0, 1, 2, 3, 3, 3))
+
+    expect_error(reind$lookup(init_keys[c(1:2)]))  			# shorter than thread count --> error
+    expect_error(reind$lookup(1:10))
+
+    expect_silent(res <- reind$lookup(init_keys))
+    expect_true(bit64::is.integer64(res))
+    expect_equal(length(init_keys), length(res))
+    expect_equal(res, exp_res)
 })
-#set_log_level("debug")
-init_map <- as.integer64(c(32, 10 ,12, 5))
-#print(init_map)
-init_keys <- as.integer64(c(5, 12, 10, 32, 10 ,12, 5, 5, 5))
-
-cat("** Calling reindex_create()\n")
-idxptr <- tiledbsoma:::reindex_create()
-
-cat("** Calling reindex_map()\n")
-idxptr <- tiledbsoma:::reindex_map(idxptr, init_map, 4)
-
-cat("** Calling reindex_lookup()\n")
-cat("** Results from reindex_lookup()\n")
-print(tiledbsoma:::reindex_lookup(idxptr, init_keys))
-print(tiledbsoma:::reindex_lookup(idxptr, init_keys[1:4]))
-print(tiledbsoma:::reindex_lookup(idxptr, rep(init_keys[1:2],2)))
-print(tiledbsoma:::reindex_lookup(idxptr, as.integer64(40:50)))
